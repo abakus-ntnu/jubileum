81de38315019f7e30ca7c6330f9e75a4d6f98778
Arash Farzaneh Taleghani <arash.f.taleghani@gmail.com>
2021-10-19 00:22:03 +0200

    update countdown component

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import {Stack,Divider, Box} from "@mui/material";
+import {Stack, Box} from "@mui/material";
 import styles from '../styles/Home.module.css'
 
 const adjustDate = (date:number) => {
@@ -8,20 +8,22 @@ const adjustDate = (date:number) => {
 
 type CountdownProps = {countDownDate:Date}
 const Countdown = ({countDownDate}: CountdownProps) => {
-    
     const [days, setDays] = useState(0);
     const [hours, setHours] = useState(0);
     const [minutes, setMinutes] = useState(0);
     const [seconds, setSeconds] = useState(0);
+    
     useEffect(() => {
-        const interval = setInterval(() => {
+        function updateTime(){
             let now = new Date().getTime();
             let distance = countDownDate.getTime() - now;
             setDays(Math.floor(distance / (1000 * 60 * 60 * 24)));
             setHours(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
             setMinutes(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
             setSeconds(Math.floor((distance % (1000 * 60)) / 1000));
-        }, 1000);
+        }
+        updateTime();
+        const interval = setInterval(updateTime, 1000);
         return () => clearInterval(interval);
       }, []);
 
