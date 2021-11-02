015e2ebdbf5f1fb0e2df6e01a2fb90e2c6c2bc2e
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-11-02 23:06:29 +0100

    Use functional component

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,35 +1,31 @@
 import { AppBar, Tabs, Tab, Toolbar } from "@mui/material";
 import React from "react";
-import { Router, withRouter } from "next/dist/client/router";
+import { useRouter } from "next/dist/client/router";
 
-class NavBar extends React.Component<{ router: Router }> {
-  handleChange(path: string): void {
-    void this.props.router.push(path);
-  }
+const NavBar = () => {
+  const router = useRouter();
 
-  render() {
-    return (
-      <AppBar position="static" sx={{ backgroundColor: "#050505" }}>
-        <Toolbar>
-          <Tabs
-            value={this.props.router.pathname}
-            onChange={(evt, newValue: string) => this.handleChange(newValue)}
-          >
-            <Tab value="/" component="a" label="Hjem" />
-            <Tab value="/ribbons" component="a" label="Daljer, Pins og Bånd" />
-            <Tab value="/readme" component="a" label="Noe fra readme" />
-            <Tab value="/leaderboard" component="a" label="Scoreboard" />
-            <Tab value="/timeline" component="a" label="Abakus Historie" />
-            <Tab
-              value="/honorary-members"
-              component="a"
-              label="Æresmedlemmer"
-            />
-          </Tabs>
-        </Toolbar>
-      </AppBar>
-    );
-  }
-}
+  const handleChange = (path: string): void => {
+    void router.push(path);
+  };
 
-export default withRouter(NavBar);
+  return (
+    <AppBar position="static" sx={{ backgroundColor: "#050505" }}>
+      <Toolbar>
+        <Tabs
+          value={router.pathname}
+          onChange={(evt, newValue: string) => handleChange(newValue)}
+        >
+          <Tab value="/" component="a" label="Hjem" />
+          <Tab value="/ribbons" component="a" label="Daljer, Pins og Bånd" />
+          <Tab value="/readme" component="a" label="Noe fra readme" />
+          <Tab value="/leaderboard" component="a" label="Scoreboard" />
+          <Tab value="/timeline" component="a" label="Abakus Historie" />
+          <Tab value="/honorary-members" component="a" label="Æresmedlemmer" />
+        </Tabs>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default NavBar;
