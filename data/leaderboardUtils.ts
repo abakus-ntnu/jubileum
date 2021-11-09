dee38e0d6c19262aefe2a590e77c1aede30b6c6f
EliHaugu <elin.haugum@hotmail.com>
2021-11-09 22:02:10 +0100

    Add leaderboard page, styles and dummy data

---
+++ data/leaderboardUtils.ts
@@ -0,0 +1,40 @@
+const participants = [
+  {
+    name: "Julie",
+    score: 60,
+  },
+  {
+    name: "Lise",
+    score: 43,
+  },
+  {
+    name: "Maracas",
+    score: 600000,
+  },
+  {
+    name: "Donald",
+    score: 100,
+  },
+  {
+    name: "Abakus",
+    score: 1,
+  },
+  {
+    name: "Bless",
+    score: 100,
+  },
+  {
+    name: "Random",
+    score: 42,
+  },
+  {
+    name: "Joe",
+    score: 9,
+  },
+  {
+    name: "lang navn på tlf, går det? jaaa lezzgo",
+    score: 99,
+  },
+];
+
+export { participants };
