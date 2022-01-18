db317aca8248b2cbd6957fbea5a69da47e90c786
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-01-19 00:45:49 +0100

    Better timeline loading

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -1,9 +1,13 @@
 import { TimelineEvent } from "models/schema";
 import {
   VerticalTimeline,
+  VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import useSWR from "swr";
 import TimelineEventElement from "./TimelineEvent";
+import Image from "next/image";
+import styles from "../styles/TimelineEvent.module.css";
+import React from "react";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
@@ -22,22 +26,46 @@ const Timeline = ({
     }
   );
 
-  if (error) return <div>Failed to load</div>;
-  if (!events) return <div>Loading...</div>;
+  if (error)
+    return (
+      <div style={{ color: "red", textAlign: "center" }}>Failed to load</div>
+    );
+  if (!events) {
+    return (
+      <VerticalTimeline>
+        {Array.from({ length: 1 }, (x, i) => i).map((i) => (
+          <VerticalTimelineElement
+            key={i}
+            date={"loading..."}
+            icon={
+              <Image
+                src="/abakule.png"
+                className={styles.timelineIcon}
+                alt=""
+                height="100px"
+                width="100px"
+              />
+            }
+          />
+        ))}
+        <div style={{ height: "100vh" }} />
+      </VerticalTimeline>
+    );
+  }
 
-  events.sort((a, b) => (a.date > b.date ? 1 : -1))
+  events.sort((a, b) => (a.date > b.date ? 1 : -1));
 
   return (
     <VerticalTimeline>
       {events.map((event, index) => (
-          <TimelineEventElement
-            key={index}
-            event={event}
-            defaultExpanded={index === 0}
-            adminPassword={adminPassword}
-            onEditClick={onEditClick}
-          />
-        ))}
+        <TimelineEventElement
+          key={index}
+          event={event}
+          defaultExpanded={index === 0}
+          adminPassword={adminPassword}
+          onEditClick={onEditClick}
+        />
+      ))}
     </VerticalTimeline>
   );
 };
