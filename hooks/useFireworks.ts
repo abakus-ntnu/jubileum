48991bfee47602626aa5b27c33c321e923a64c6e
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-02-03 00:45:17 +0100

    🎉 confetti fireworks 🎉

---
+++ hooks/useFireworks.ts
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import confetti from "canvas-confetti";
+
+function useFireworks() {
+  useEffect(() => {
+    const duration = 15 * 1000;
+    const animationEnd = Date.now() + duration;
+    const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    function randomInRange(min: number, max: number): number {
+      return Math.random() * (max - min) + min;
+    }
+
+    const interval: ReturnType<typeof setInterval> = setInterval(function () {
+      const timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+
+      const particleCount = 50 * (timeLeft / duration);
+      // since particles fall down, start a bit higher than random
+      void confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      });
+      void confetti({
+        ...defaults,
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      });
+    }, 250);
+  }, []);
+}
+
+export default useFireworks;
